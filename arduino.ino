ef52324bd7db347dd843ed91b225f8eb7a1914bd
Guilherme Athayde <guiathayde303@gmail.com>
2022-10-20 17:39:19 -0300

    Initial commit

--- arduino.ino
+++ arduino.ino
@@ -1,22 +1,17 @@
 #include <SoftwareSerial.h> //INCLUSÃO DE BIBLIOTECA
 
-const int pinoRX = 2;    // PINO DIGITAL 2 (RX)
-const int pinoTX = 3;    // PINO DIGITAL 3 (TX)
-const int pinoLed = 12;  // PINO DIGITAL UTILIZADO PELO LED
-int dadoBluetooth = 0;   // VARIÁVEL QUE ARMAZENA O VALOR ENVIADO PELO BLUETOOTH
-boolean loopLED = false; // VARIÁVEL BOOLEANA QUE FAZ O CONTROLE DE ATIVAÇÃO DO LOOP DO LED
+const int pinoRX = 2;  // PINO DIGITAL 2 (RX)
+const int pinoTX = 3;  // PINO DIGITAL 3 (TX)
+int dadoBluetooth = 0; // VARIÁVEL QUE ARMAZENA O VALOR ENVIADO PELO BLUETOOTH
 
 SoftwareSerial bluetooth(pinoRX, pinoTX); // PINOS QUE EMULAM A SERIAL, ONDE
 // O PINO 2 É O RX E O PINO 3 É O TX
 
 void setup()
 {
-  Serial.begin(9600);       // INICIALIZA A SERIAL
-  bluetooth.begin(9600);    // INICIALIZA A SERIAL DO BLUETOOTH
-  bluetooth.print("$");     // IMPRIME O CARACTERE
-  bluetooth.print("$");     // IMPRIME O CARACTERE
-  bluetooth.print("$");     // IMPRIME O CARACTERE
-  pinMode(pinoLed, OUTPUT); // DEFINE O PINO COMO SAÍDA
+  Serial.begin(9600);    // INICIALIZA A SERIAL
+  bluetooth.begin(9600); // INICIALIZA A SERIAL DO BLUETOOTH
+  Serial.println("The bluetooth gates are open.\n Connect to HC-05 from any other bluetooth device with 1234 as pairing key!");
 }
 
 void loop()
@@ -24,11 +19,6 @@ void loop()
   if (bluetooth.available())
   {                                   // SE O BLUETOOTH ESTIVER HABILITADO, FAZ
     dadoBluetooth = bluetooth.read(); // VARIÁVEL RECEBE O VALOR ENVIADO PELO BLUETOOTH
-
-    if (dadoBluetooth == 'm')
-    {
-      Serial.println("Set-up Vodka");
-    }
 
     // Vodka sprite
     if (dadoBluetooth == 'a')
@@ -46,6 +36,24 @@ void loop()
     if (dadoBluetooth == 'c')
     {
       Serial.println("Cosmo in the making");
+    }
+
+    // Lemon drop
+    if (dadoBluetooth == 'd')
+    {
+      Serial.println("Lemon drop in the making");
+    }
+
+    // Blue Moon
+    if (dadoBluetooth == 'e')
+    {
+      Serial.println("Blue Moon in the making");
+    }
+
+    // Blue Gin Moon
+    if (dadoBluetooth == 'f')
+    {
+      Serial.println("Blue Gin Moon in the making");
     }
   }
 }
